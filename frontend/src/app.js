3581f37b5e6a63ecd81dfa79f177e8fa1325f92f
Tobias Dyhrberg <tob.jensen@gmail.com>
2019-07-24 15:31:49 +1000

    implemented localStorage for favourite persistance

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { fetchFerryData, addFavourite } from './actions/ferries';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(fetchFerryData());
-store.dispatch(addFavourite('Friendship'));
-store.dispatch(addFavourite('Alexander'));
-store.dispatch(fetchFerryData());
 
 const jsx = (
   <Provider store={store}>
--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import ferriesReducer from '../reducers/ferries';
 import searchReducer from '../reducers/search';
+import { fetchFerryData, updateFerryData } from '../actions/ferries';
 
 export default () => {
   const store = createStore(
@@ -11,5 +12,16 @@ export default () => {
     }),
     applyMiddleware(ReduxThunk)
   );
+
+  const localFerries = JSON.parse(localStorage.getItem('ferries'))
+  store.dispatch(updateFerryData(localFerries))
+
+  store.subscribe(() => {
+	const json = JSON.stringify(store.getState()['ferries'])
+	localStorage.setItem('ferries', json)
+	})
+
+  store.dispatch(fetchFerryData());
+
   return store;
 };
